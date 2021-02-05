7ad8093918bc5a4ea95bec899cd66b4a8e00da91
example <alisayar0107@gmail.com>
2021-02-05 20:44:26 +0300

    modal closing with clicking outer modal functionality added.

--- script.js
+++ script.js
@@ -15,3 +15,4 @@ function showModal() {
 // Modal event listeners 
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false) )
