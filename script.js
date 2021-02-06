cf9f71759f70c38ef2de9036a34c4704bbd27e57
example <alisayar0107@gmail.com>
2021-02-06 18:47:22 +0300

    regular expression added

--- script.js
+++ script.js
@@ -17,11 +17,28 @@ modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false) )
 
-
+// Validate form with regular expression
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if(urlValue.match(regex)) {
+        alert('match');
+    }
+    if(!urlValue.match(regex)) {
+        alert('Please provide a valid web address')
+    }
+}
+ 
 // Handle data from form
 function storeBookmark(e) {
-    console.log(e);
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if(!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    console.log(nameValue, urlValue);
 }
 
 // Event Listener
-bookmarkForm.addEventListener('submit', storeBookmark)
+bookmarkForm.addEventListener('submit', storeBookmark);
