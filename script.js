b61476c5c5fa04773edccd97a45876ded850b32d
example <alisayar0107@gmail.com>
2021-02-07 15:45:47 +0300

    now items of bookmars created dynamically by buildBookmarks function

--- script.js
+++ script.js
@@ -40,7 +40,30 @@ function buildBookmarks() {
     // Build items
     bookmarks.forEach((bookmark) => {
       const { name, url} = bookmark;
-      
+      // Item
+      const item = document.createElement('div');
+      item.classList.add('item');
+      // Close icon
+      const closeIcon = document.createElement('i');
+      closeIcon.classList.add('fas', 'fa-times-circle');
+      closeIcon.setAttribute('title', 'Delete bookmark!');
+      closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+      // Favicon / Link container
+      const linkInfo = document.createElement('div');
+      linkInfo.classList.add('name');
+      // Favicon
+      const favicon = document.createElement('img');
+      favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+      favicon.setAttribute('alt', 'favicon');
+      // Link
+      const link = document.createElement('a');
+      link.setAttribute('href', `${url}`);
+      link.setAttribute('target', '_blank');
+      link.textContent = name;
+      // Append to ookmarks container
+      linkInfo.append(favicon, link);
+      item.append(closeIcon, linkInfo);
+      bookmarksContainer.appendChild(item);
     });
 }
 
